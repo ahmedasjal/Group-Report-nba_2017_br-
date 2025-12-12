389993d60eda57d676c50f97d55bdce0cdf1974c
Ahmed Asjal <aa24aux@herts.ac.uk>
2025-12-12 15:12:53 +0000

    developed the scatter plot code with the team

--- RCODE.R
+++ RCODE.R
@@ -13,3 +13,19 @@ nba_clean <- nba %>%
   select(Player, Pos, MP, `PS/G`) %>%
   filter(!is.na(MP), !is.na(`PS/G`)) %>%
   rename(PSG = `PS/G`)
+
+#  Scatterplot: PS/G on X-axis, MP on Y-axis
+
+scatter_plot <- nba_clean %>%
+  ggplot(aes(x = PSG, y = MP, colour = Pos)) +
+  geom_point(alpha = 0.7) +
+  geom_smooth(method = "lm", se = TRUE, colour = "black") +
+  labs(
+    title = "Scatter Plot: Points Scored per Game (PS/G) vs Minutes Played (MP)",
+    x = "Points Scored per Game (PS/G)",
+    y = "Minutes Played (MP)",
+    colour = "Position"
+  ) +
+  theme_minimal()
+
+print(scatter_plot)
